590b5d5341bf01dc607b9b26f8d590eb9952b142
Szymon Lechniak <szymonlechniak@gmail.com>
2023-05-10 13:58:16 +0200

    draft 1

--- MinimalAPI1/Program.cs
+++ MinimalAPI1/Program.cs
@@ -3,16 +3,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-
 #region cennik
 
-app.MapGet("/priceitems", async () =>
+var priceItemGroup = app.MapGroup("/priceitem");
+
+priceItemGroup.MapGet("/", async () =>
 {
     return await GetPriceItems();
 });
 
-app.MapGet("/priceitem/{id}", async (int id) =>
+priceItemGroup.MapGet("/{id}", async (int id) =>
 {
     PriceItem item = await GetPriceItem(id);
     if (item is null)
@@ -20,7 +20,7 @@ app.MapGet("/priceitem/{id}", async (int id) =>
     return Results.Ok(item);
 });
 
-app.MapDelete("/priceitem/{id}", async (int id) =>
+priceItemGroup.MapDelete("/{id}", async (int id) =>
 {
     PriceItem item = await DeletePriceItem(id);
     if (item is null)
@@ -28,7 +28,7 @@ app.MapDelete("/priceitem/{id}", async (int id) =>
     return Results.Ok(item);
 });
 
-app.MapPost("/priceitem", async (PriceItem priceItem) =>
+priceItemGroup.MapPost("/", async (PriceItem priceItem) =>
 {
     PriceItem item = await CreatePriceItem(priceItem);
     if (item is null)
@@ -36,9 +36,11 @@ app.MapPost("/priceitem", async (PriceItem priceItem) =>
     return Results.Created($"/priceitem/{item.Id}", item);
 });
 
-app.MapPut("/priceitem", async (PriceItem priceItem) =>
+priceItemGroup.MapPut("/", async (PriceItem priceItem) =>
 {
-    await UpdatePriceItem(priceItem);
+    PriceItem item = await UpdatePriceItem(priceItem);
+    if (item is null)
+        return Results.BadRequest();
     return Results.NoContent();
 });
 
@@ -59,37 +61,31 @@ app.Run();
 static async Task<List<SalesItem>> GetSalesItems()
 {
     //fetch the list of sales items and return it
-    return new List<SalesItem>() {
-        new SalesItem { Id = 0, Date = new DateTime(2023,6,10,12,30,22), Sum = 10.99m},
-        new SalesItem { Id = 1, Date = new DateTime(2023,3,17,14,24,13), Sum = 20.99m } };
-    //return null;
+    return null;
 }
 static async Task<List<PriceItem>> GetPriceItems() 
 {
     //fetch the list of price items and return it
-    return new List<PriceItem>() {
-        new PriceItem { Id = 0, Name = "test0", Price = 0.99m},
-        new PriceItem { Id = 1, Name = "test1", Price = 1.99m } };
-    //return null;
+    return null;
 }
 static async Task<PriceItem> GetPriceItem(int id)
 {
-    //fetch price item and return it
-    return new List<PriceItem>() {
-        new PriceItem { Id = 0, Name = "test0", Price = 0.99m},
-        new PriceItem { Id = 1, Name = "test1", Price = 1.99m } }.FirstOrDefault(x=>x.Id==id);
-    //return null;
+    //fetch price item and return it, if not found return null
+    return null;
 }
 static async Task<PriceItem> CreatePriceItem(PriceItem priceItem) 
 {
-    //add price item
-    return priceItem;
-    //return null;
+    //add price item and return it, if unsuccessful return null
+    return null;
 }
-static async Task UpdatePriceItem(PriceItem priceItem) { }
+static async Task<PriceItem> UpdatePriceItem(PriceItem priceItem)
+{
+    //find price item, update and return it, if not found return null
+    return null;
+}
 static async Task<PriceItem> DeletePriceItem(int id) 
 {
-    //find item, delete and return it, if not found return null
+    //find price item, delete and return it, if not found return null
     return null;
 }
 
